fac3524c9045ab083c83370f81c6fbf64b51b9da
leila52 <lailahaddad385@gmail.com>
2024-02-14 13:47:30 +0100

    444

---
+++ javascript/Soluciones/PiedraPapelTijera/funciones.js
@@ -0,0 +1,38 @@
+let puntos_usu = 0;
+let puntos_ord = 0;
+let puntos_usu_span;
+let puntos_ord_span;
+let resultado;
+let img_Ord;
+
+this.onload = function () {
+    iniciar();
+}
+function iniciar() {
+    //Asignamos a las variables globales declaradas los elementos HTML
+    
+}
+function jugar(jugada_usu) {
+
+    //El usuario lanza su jugada y generamos la jugada del ordenador
+   
+}
+function obtenerJug_Ordenador() {
+   //generamos la jugada del ordenador
+   //pista: utiliza arrays y Math.Random
+}
+function jugador_Gana(jugada_usu, jugada_ord) {
+   //Si el jugador gana mostramos el texto correspondiente dependiendo de los argumentos que nos llegan,
+   // por ejemplo "Piedra gana tijeras"
+}
+function jugador_Pierde(jugada_usu, jugada_ord) {
+    //Si el jugador pierde mostramos el texto correspondiente dependiendo de los argumentos que nos llegan,
+   // por ejemplo "Piedra pierde con papel"
+   
+}
+function empate() {
+    //mostramos el texto empate
+}
+function mostrarNombre(id) {
+   //convertimos la r en piedra, la p en papel y la t en tijeras
+}
